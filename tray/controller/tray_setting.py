f44f6972cfdb2e2d88e00da5b2a74742f00a6fa8
originalMemory <kdi1994@163.com>
2020-02-16 23:24:04 +0800

    ADD: 增加顺序和随机两种切换选项

--- tray/controller/tray_setting.py
+++ tray/controller/tray_setting.py
@@ -14,6 +14,7 @@ import os
 import random
 import threading
 import time
+from enum import Enum, unique
 from functools import partial
 
 import pyperclip
@@ -25,7 +26,18 @@ from helper.db_helper import DBHelper
 from tray.view.tray_setting import Ui_TraySetting
 
 
+@unique
+class ChangeType(Enum):
+    Order = 0
+    Random = 1
+
+
 class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
+
+    __config_section_history = "history"
+    __config_section_background = "background"
+    __config_key_change_type = "changeType"
+    __config_key_last_order_image_offset = "lastOrderImageOffset"
 
     def __init__(self, parent=None):
         super(TraySetting, self).__init__(parent)
@@ -34,9 +46,9 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
         # 初始化数据
         self.__db_helper = DBHelper(self)
         self.__config_helper = ConfigHelper(self)
-        self.__sql_where = self.__config_helper.get_config_key("background", "sqlWhere")
+        self.__sql_where = self.__config_helper.get_config_key(self.__config_section_background, "sqlWhere")
         self.textEdit_sqlWhere.setText(self.__sql_where)
-        self.__time_interval = int(self.__config_helper.get_config_key("background", "timeIntervalInMin"))
+        self.__time_interval = int(self.__config_helper.get_config_key(self.__config_section_background, "timeIntervalInMin"))
         self.lineEdit_min.setText(str(self.__time_interval))
 
         self.__current_image = None
@@ -65,6 +77,25 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
             level_action.triggered.connect(partial(self.__set_level, level.id))
             self.__level_actions.append(level_action)
 
+        # 壁纸切换方式
+        menu.addSeparator()
+        self.__change_type_actions = list()
+        self.create_change_type_menu(menu)
+        # 加载默认参数
+        type_value = self.__config_helper.get_config_key(
+            self.__config_section_background,
+            self.__config_key_change_type,
+            ChangeType.Order.value
+        )
+        self.__change_type = ChangeType(int(type_value))
+        self.__update_change_type_action(self.__change_type)
+        offset = self.__config_helper.get_config_key(
+            self.__config_section_background,
+            self.__config_key_last_order_image_offset,
+            0
+        )
+        self.__last_order_image_offset = int(offset)
+
         menu.addSeparator()
         setting = QtWidgets.QAction("设置", self)
         setting.triggered.connect(self.show)
@@ -77,20 +108,61 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
 
         threading.Thread(target=self.__change_windows_background, daemon=True).start()
 
+    def create_change_type_menu(self, menu):
+        change_type_menu = menu.addMenu("切换方式")
+        change_order = self.__create_type_action("顺序", ChangeType.Order)
+        change_type_menu.addAction(change_order)
+        self.__change_type_actions.append(change_order)
+        change_random = self.__create_type_action("随机", ChangeType.Random)
+        change_type_menu.addAction(change_random)
+        self.__change_type_actions.append(change_random)
+
+    def __create_type_action(self, name, change_type):
+        action = QtWidgets.QAction(name, self)
+        action.setCheckable(True)
+        action.triggered.connect(partial(self.__set_change_type, change_type))
+        return action
+
+    def __set_change_type(self, change_type: ChangeType):
+        self.__change_type = change_type
+        self.__update_change_type_action(change_type)
+
+    def __update_change_type_action(self, change_type: ChangeType):
+        if change_type == ChangeType.Order:
+            self.__change_type_actions[0].setChecked(True)
+            self.__change_type_actions[1].setChecked(False)
+        elif change_type == ChangeType.Random:
+            self.__change_type_actions[0].setChecked(False)
+            self.__change_type_actions[1].setChecked(True)
+        self.__config_helper.add_config_key(
+            self.__config_section_background,
+            self.__config_key_change_type,
+            change_type.value
+        )
+
     def __save(self):
         self.__sql_where = self.textEdit_sqlWhere.toPlainText()
         time_interval = self.lineEdit_min.text()
         self.__time_interval = int(time_interval)
         self.__config_helper.add_config_key("background", "sqlWhere", self.__sql_where)
         self.__config_helper.add_config_key("background", "timeIntervalInMin", time_interval)
+        self.__last_order_image_offset = int(self.lineEdit_order_offset.text())
+        self.__config_helper.add_config_key(
+            self.__config_section_background,
+            self.__config_key_last_order_image_offset,
+            self.__last_order_image_offset
+        )
         self.hide()
 
     def __change_windows_background(self):
         SPI_SETDESKWALLPAPER = 20
         while True:
-            image_count = self.__db_helper.get_image_count(self.__sql_where)
-            offset = random.randint(0, image_count)
             sleep_second = self.__time_interval * 60
+            image_count = self.__db_helper.get_image_count(self.__sql_where)
+            if self.__change_type == ChangeType.Order:
+                offset = self.__get_order_offset(image_count)
+            else:
+                offset = random.randint(0, image_count)
             image = self.__db_helper.get_one_image_with_where(self.__sql_where, offset)
             if not image:
                 QMessageBox.information(self, "提示", "sql 语句限制过多，获取不到图片", QMessageBox.Ok)
@@ -103,12 +175,28 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
             print(image)
             # 更换背景图
             ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image.path, 0)
-            image_info = f"{image.author} - {image.filename}"
+
+            image_info = f"[{offset}/{image_count}] {image.author} - {image.filename}"
             if len(image_info) > 50:
                 image_info = f"{image_info[0:46]}..."
             self.__current_image_action.setText(image_info)
             self.__update_level_action(image.level_id)
             time.sleep(sleep_second)
+
+    def __get_order_offset(self, image_count):
+        if self.__last_order_image_offset < image_count - 1:
+            offset = self.__last_order_image_offset + 1
+        else:
+            offset = 0
+        self.__last_order_image_offset = offset
+        self.__config_helper.add_config_key(
+            self.__config_section_history,
+            self.__config_key_last_order_image_offset,
+            self.__last_order_image_offset
+        )
+        if self.isHidden():
+            self.lineEdit_order_offset.setText(str(offset))
+        return offset
 
     def __update_level_action(self, level_id):
         for i in range(len(self.__levels)):
