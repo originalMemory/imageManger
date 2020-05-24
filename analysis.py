26fd00c3dff1d120873331183975d5e7d88e06e0
originalMemory <kdi1994@163.com>
2020-05-24 10:20:41 +0800

    ADD: google 翻译 yande 标签

--- analysis.py
+++ analysis.py
@@ -10,8 +10,13 @@
 @update  :
 """
 
+from googletrans import Translator
 from openpyxl import Workbook
+from openpyxl import load_workbook
 from helper.db_helper import DBHelper
+from json import JSONDecodeError
+from random import randint
+import time
 
 
 def extractYandeTags():
@@ -52,5 +57,39 @@ def error_handler(error_str):
     print(f"数据库错误：{error_str}")
 
 
+def translate(startRow):
+    filename = "yande标签列表.xlsx"
+    wb = load_workbook(filename)
+    sheet = wb.get_active_sheet()
+    tags = []
+    translator = Translator(service_urls=['translate.google.cn'])
+    count = 300
+    while startRow < sheet.max_row:
+        print(f"翻译 {startRow}/{sheet.max_row}")
+        for i in range(0, count):
+            currentRow = startRow + i
+            if currentRow > sheet.max_row:
+                break
+            tags.append(sheet.cell(currentRow, 2).value)
+        src = "，".join(tags)
+        try:
+            translates = translator.translate(src, dest='zh-CN',src='en')
+            dests = translates.text.split("，")
+            for index, text in enumerate(dests):
+                print(f"[{startRow + index}/{sheet.max_row}] ", tags[index], ' -> ', text)
+                sheet.cell(startRow + index, 3).value = text
+            time.sleep(random())
+        except JSONDecodeError as error:
+            print("到达上限")
+            break
+        startRow += count
+        tags.clear()
+
+    wb.save(filename)
+
+def random():
+  random_number =randint(0, 10)
+  return random_number
+
 if __name__ == '__main__':
-    extractYandeTags()
+    translate(2)
