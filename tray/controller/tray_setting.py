b70f89532decc868664d4980e4eb73fac2ab73ed
wuhb <wuhb@yuanfudao.com>
2023-01-03 17:38:23 +0800

    RFCT: __ 属性和方法重命名为 _

--- tray/controller/tray_setting.py
+++ tray/controller/tray_setting.py
@@ -39,68 +39,68 @@ class ChangeType(Enum):
 
 
 class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
-    __config_section_background = "background"
-    __config_key_change_type = "changeType"
-    __config_key_last_order_image_offset = "lastOrderImageOffset"
+    _config_section_background = "background"
+    _config_key_change_type = "changeType"
+    _config_key_last_order_image_offset = "lastOrderImageOffset"
     _monitor_start_y = 0
     _monitor_settings = []  # 显示器设定列表
 
-    def __init__(self, parent=None):
-        super(TraySetting, self).__init__(parent)
+    def _init_(self, parent=None):
+        super(TraySetting, self)._init_(parent)
         self.setupUi(self)
 
         # 初始化数据
-        self.__db_helper = DBHelper(self)
-        self.__config_helper = ConfigHelper(self)
-        fl = self.__config_helper.get_config_key(self.__config_section_background, "filter")
+        self._db_helper = DBHelper(self)
+        self._config_helper = ConfigHelper(self)
+        fl = self._config_helper.get_config_key(self._config_section_background, "filter")
         if fl:
             fl = fl.replace('\\"', '"')[1:-1]
             self._fl = json.loads(fl)
             self.textEdit_sqlWhere.setText(fl)
         else:
             self._fl = {}
-        self.__time_interval = int(
-            self.__config_helper.get_config_key(self.__config_section_background, "timeIntervalInMin"))
-        self.lineEdit_min.setText(str(self.__time_interval))
+        self._time_interval = int(
+            self._config_helper.get_config_key(self._config_section_background, "timeIntervalInMin"))
+        self.lineEdit_min.setText(str(self._time_interval))
 
-        self.__current_image = None
+        self._current_image = None
 
-        self.pushButton_save.pressed.connect(self.__save)
+        self.pushButton_save.pressed.connect(self._save)
         self.pushButton_cancel.pressed.connect(self.hide)
 
         # 系统托盘
-        self.__tray = QtWidgets.QSystemTrayIcon()
-        self.__tray.setIcon(QtGui.QIcon("images/tranIcon.png"))
-        self.__tray.setToolTip("壁纸切换")
-        # self.__tray.activated[QSystemTrayIcon.ActivationReason].connect(self.__on_tray_click)
+        self._tray = QtWidgets.QSystemTrayIcon()
+        self._tray.setIcon(QtGui.QIcon("images/tranIcon.png"))
+        self._tray.setToolTip("壁纸切换")
+        # self._tray.activated[QSystemTrayIcon.ActivationReason].connect(self._on_tray_click)
         menu = QtWidgets.QMenu()
 
         # 创建显示器对应壁纸项
         self._monitors = get_monitors()
-        self.__levels = self.__db_helper.get_model_data_list('level')
+        self._levels = self._db_helper.get_model_data_list('level')
         self.create_monitor_menu(menu)
 
         # 壁纸切换方式
-        self.__change_type_actions = list()
+        self._change_type_actions = list()
         self.create_change_type_menu(menu)
 
         switch_next = QtGui.QAction("切换下一张", self)
-        switch_next.triggered.connect(self.__change_background)
+        switch_next.triggered.connect(self._change_background)
         menu.addAction(switch_next)
         # 加载默认参数
-        type_value = self.__config_helper.get_config_key(
-            self.__config_section_background,
-            self.__config_key_change_type,
+        type_value = self._config_helper.get_config_key(
+            self._config_section_background,
+            self._config_key_change_type,
             ChangeType.Order.value
         )
-        self.__change_type = ChangeType(int(type_value))
-        self.__update_change_type_action(self.__change_type)
-        offset = self.__config_helper.get_config_key(
-            self.__config_section_background,
-            self.__config_key_last_order_image_offset,
+        self._change_type = ChangeType(int(type_value))
+        self._update_change_type_action(self._change_type)
+        offset = self._config_helper.get_config_key(
+            self._config_section_background,
+            self._config_key_last_order_image_offset,
             0
         )
-        self.__last_order_image_offset = int(offset)
+        self._last_order_image_offset = int(offset)
         self.lineEdit_order_offset.setText(str(offset))
 
         menu.addSeparator()
@@ -110,10 +110,10 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
         close = QtGui.QAction("退出", self)
         close.triggered.connect(self.close)
         menu.addAction(close)
-        self.__tray.setContextMenu(menu)
-        self.__tray.show()
+        self._tray.setContextMenu(menu)
+        self._tray.show()
 
-        threading.Thread(target=self.__change_windows_background_timely, daemon=True).start()
+        threading.Thread(target=self._change_windows_background_timely, daemon=True).start()
 
     def create_monitor_menu(self, menu):
         """
@@ -126,7 +126,7 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
         for i in range(len(monitors)):
             monitor = monitors[i]
             desc_action = QtGui.QAction("", self)
-            desc_action.triggered.connect(partial(self.__open_file_in_directory_and_copy_file_name, i))
+            desc_action.triggered.connect(partial(self._open_file_in_directory_and_copy_file_name, i))
             menu.addAction(desc_action)
 
             if monitor.width > monitor.height:
@@ -135,10 +135,10 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
                 screen_state = '竖屏'
             level_menu = menu.addMenu(f" {monitor.name} {screen_state} - 等级")
             level_actions = []
-            for level in self.__levels:
+            for level in self._levels:
                 level_action = level_menu.addAction(level.name)
                 level_action.setCheckable(True)
-                level_action.triggered.connect(partial(self.__set_level, i, level.id))
+                level_action.triggered.connect(partial(self._set_level, i, level.id))
                 level_actions.append(level_action)
             menu.addSeparator()
 
@@ -150,59 +150,59 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
 
     def create_change_type_menu(self, menu):
         change_type_menu = menu.addMenu("切换方式")
-        change_order = self.__create_type_action("顺序", ChangeType.Order)
+        change_order = self._create_type_action("顺序", ChangeType.Order)
         change_type_menu.addAction(change_order)
-        self.__change_type_actions.append(change_order)
-        change_random = self.__create_type_action("随机", ChangeType.Random)
+        self._change_type_actions.append(change_order)
+        change_random = self._create_type_action("随机", ChangeType.Random)
         change_type_menu.addAction(change_random)
-        self.__change_type_actions.append(change_random)
+        self._change_type_actions.append(change_random)
 
-    def __create_type_action(self, name, change_type):
+    def _create_type_action(self, name, change_type):
         action = QtGui.QAction(name, self)
         action.setCheckable(True)
-        action.triggered.connect(partial(self.__set_change_type, change_type))
+        action.triggered.connect(partial(self._set_change_type, change_type))
         return action
 
-    def __set_change_type(self, change_type: ChangeType):
-        self.__change_type = change_type
-        self.__update_change_type_action(change_type)
+    def _set_change_type(self, change_type: ChangeType):
+        self._change_type = change_type
+        self._update_change_type_action(change_type)
 
-    def __update_change_type_action(self, change_type: ChangeType):
+    def _update_change_type_action(self, change_type: ChangeType):
         if change_type == ChangeType.Order:
-            self.__change_type_actions[0].setChecked(True)
-            self.__change_type_actions[1].setChecked(False)
+            self._change_type_actions[0].setChecked(True)
+            self._change_type_actions[1].setChecked(False)
         elif change_type == ChangeType.Random:
-            self.__change_type_actions[0].setChecked(False)
-            self.__change_type_actions[1].setChecked(True)
-        self.__config_helper.add_config_key(
-            self.__config_section_background,
-            self.__config_key_change_type,
+            self._change_type_actions[0].setChecked(False)
+            self._change_type_actions[1].setChecked(True)
+        self._config_helper.add_config_key(
+            self._config_section_background,
+            self._config_key_change_type,
             change_type.value
         )
 
-    def __save(self):
+    def _save(self):
         fl = self.textEdit_sqlWhere.toPlainText()
         self._fl = json.loads(fl)
         time_interval = self.lineEdit_min.text()
-        self.__time_interval = int(time_interval)
-        self.__config_helper.add_config_key("background", "filter", json.dumps(fl))
-        self.__config_helper.add_config_key("background", "timeIntervalInMin", time_interval)
-        self.__last_order_image_offset = int(self.lineEdit_order_offset.text())
-        self.__config_helper.add_config_key(
-            self.__config_section_background,
-            self.__config_key_last_order_image_offset,
-            self.__last_order_image_offset
+        self._time_interval = int(time_interval)
+        self._config_helper.add_config_key("background", "filter", json.dumps(fl))
+        self._config_helper.add_config_key("background", "timeIntervalInMin", time_interval)
+        self._last_order_image_offset = int(self.lineEdit_order_offset.text())
+        self._config_helper.add_config_key(
+            self._config_section_background,
+            self._config_key_last_order_image_offset,
+            self._last_order_image_offset
         )
         self.hide()
 
-    def __change_windows_background_timely(self):
+    def _change_windows_background_timely(self):
         while True:
-            sleep_second = self.__time_interval * 60
-            if self.__change_background():
+            sleep_second = self._time_interval * 60
+            if self._change_background():
                 print(f'睡眠{sleep_second}s')
                 time.sleep(sleep_second)
 
-    def __change_background(self):
+    def _change_background(self):
         """
         修改桌面壁纸
         :return:
@@ -222,7 +222,7 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
             if len(desc) > 50:
                 desc = f"{desc[0:46]}..."
             setting.image_desc_action.setText(desc)
-            self.__update_level_action(i, image.level)
+            self._update_level_action(i, image.level)
 
             try:
                 image_data = ImageHelper.get_sized_image(
@@ -262,38 +262,38 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
             fl['$expr'] = {'$gte': ['$width', '$height']}
         else:
             fl['$expr'] = {'$lte': ['$width', '$height']}
-        image_count = self.__db_helper.get_count(fl)
+        image_count = self._db_helper.get_count(fl)
         if not image_count:
             return
-        if self.__change_type == ChangeType.Order:
-            offset = self.__get_order_offset(image_count)
+        if self._change_type == ChangeType.Order:
+            offset = self._get_order_offset(image_count)
         else:
             offset = random.randint(0, image_count)
-        img = MyImage.from_dict(self.__db_helper.img_col.find(fl).skip(offset).limit(1)[0])
+        img = MyImage.from_dict(self._db_helper.img_col.find(fl).skip(offset).limit(1)[0])
         print(f'where: {json.dumps(fl)}, id: {img.id}, width: {img.width}, height: {img.height}, path: {img.path}')
         return img, offset, image_count
 
-    def __get_order_offset(self, image_count):
+    def _get_order_offset(self, image_count):
         """
         获取顺序切换时下一张图片的偏移量
         :param image_count: 图片总数
         :return: 偏移量
         """
-        if self.__last_order_image_offset < image_count - 1:
-            offset = self.__last_order_image_offset + 1
+        if self._last_order_image_offset < image_count - 1:
+            offset = self._last_order_image_offset + 1
         else:
             offset = 0
-        self.__last_order_image_offset = offset
-        self.__config_helper.add_config_key(
-            self.__config_section_background,
-            self.__config_key_last_order_image_offset,
-            self.__last_order_image_offset
+        self._last_order_image_offset = offset
+        self._config_helper.add_config_key(
+            self._config_section_background,
+            self._config_key_last_order_image_offset,
+            self._last_order_image_offset
         )
         if self.isHidden():
             self.lineEdit_order_offset.setText(str(offset))
         return offset
 
-    def __update_level_action(self, index, level):
+    def _update_level_action(self, index, level):
         """
         更新当前显示器壁纸等级状态
         :param index: 显示器索引
@@ -301,24 +301,24 @@ class TraySetting(QtWidgets.QWidget, Ui_TraySetting):
         :return:
         """
         level_actions = self._monitor_settings[index].image_level_actions
-        for i in range(len(self.__levels)):
-            level = self.__levels[i]
+        for i in range(len(self._levels)):
+            level = self._levels[i]
             level_actions[i].setChecked(level.id == level)
 
-    def __on_tray_click(self, reason: QSystemTrayIcon.ActivationReason):
+    def _on_tray_click(self, reason: QSystemTrayIcon.ActivationReason):
         if reason == QSystemTrayIcon.Trigger:
             self.show()
             return
 
-    def __open_file_in_directory_and_copy_file_name(self, index):
+    def _open_file_in_directory_and_copy_file_name(self, index):
         image = self._monitor_settings[index].image
         file_path = image.full_path().replace('/', '\\')
         ex = f"explorer /select,{file_path}"
         os.system(ex)
         pyperclip.copy(image.filename)
 
-    def __set_level(self, index, level):
+    def _set_level(self, index, level):
         image = self._monitor_settings[index].image
         image.level = level
-        self.__db_helper.update_image(image)
-        self.__update_level_action(index, level)
+        self._db_helper.update_image(image)
+        self._update_level_action(index, level)
